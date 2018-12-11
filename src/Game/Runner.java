d95dd2527770831ca3fa651b04e03c881bba7e48
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-12-10 22:17:01 -0500

    Initial commit

---
+++ src/Game/Runner.java
@@ -0,0 +1,12 @@
+package Game;
+
+import java.util.Scanner;
+
+public class Runner {
+    boolean gameOn = true;
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Rooms/Room.java
@@ -0,0 +1,18 @@
+package Rooms;
+
+public class Room {
+    private int xLoc,yLoc;
+    public Room(int xLoc, int yLoc)
+    {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
+    public int getxLoc()
+    {
+        return xLoc;
+    }
+    public int getyLoc()
+    {
+        return yLoc;
+    }
+}
